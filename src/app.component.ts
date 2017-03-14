2bb1c1a4e552426f4726bcc2df8dc5d8853e28be
Nova Framework <mitch.lamers@planonsoftware.com>
2017-03-14 08:34:02 +0100

    Default routing example

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app',
-    template: `<todo-list></todo-list>`,
+    selector: 'my-app',
+    template: `
+    <h1>Angular Router</h1>
+    <nav>
+      <a routerLink="/crisis-center" routerLinkActive="active">Crisis Center</a>
+      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `
 })
-export class AppComponent {}
+export class AppComponent { }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,31 @@
+import { PageNotFoundComponent } from './not-found.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { MaterialModule } from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
-import { TodoListComponent } from './todo/todo-list.component';
-import { TodoService } from './todo/todo.service';
-import { TodoFilter } from './todo/todo-filter.pipe';
-import { TodoItemComponent } from './todo/todo-item.component';
+import { CrisisListComponent } from './crisis-list.component';
+import { HeroListComponent } from './hero-list.component';
+
+const appRoutes: Routes = [
+    { path: 'crisis-center', component: CrisisListComponent },
+    { path: 'heroes', component: HeroListComponent },
+    { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
-        MaterialModule
+        RouterModule.forRoot(appRoutes)
     ],
     declarations: [
         AppComponent,
-        TodoListComponent,
-        TodoFilter,
-        TodoItemComponent
+        HeroListComponent,
+        CrisisListComponent,
+        PageNotFoundComponent
     ],
-    providers: [TodoService],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
